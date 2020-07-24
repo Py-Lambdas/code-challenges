b90bb0e7d039e35292a02d771adaf42e01f403cf
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-07-24 17:38:22 -0400

    Added perimeter of rectangle challenge

---
+++ Perimeter_of_Rectangle/find_perimeter_of_rectangle.py
@@ -0,0 +1,15 @@
+"""
+Challenge: Create a function that takes length and width and finds the perimeter of a rectangle.
+Difficulty: Easy
+
+Examples:
+find_perimeter(6, 7) ➞ 26
+find_perimeter(20, 10) ➞ 60
+find_perimeter(2, 9) ➞ 22
+
+Author: @joshrutkowski
+"""
+
+
+def find_perimeter(length, width):
+    pass  # Your code here
