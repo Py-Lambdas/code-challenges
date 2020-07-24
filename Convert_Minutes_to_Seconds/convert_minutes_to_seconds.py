6cd60280135699a62d253ef3197e7047fb7ce259
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-07-24 14:48:44 -0400

    Added new challenge

---
+++ Convert_Minutes_to_Seconds/convert_minutes_to_seconds.py
@@ -0,0 +1,15 @@
+"""
+Challenge: Write a function that takes an integer minutes and converts it to seconds.
+Difficulty: Very Easy
+
+Examples
+convert(5) ➞ 300
+convert(3) ➞ 180
+convert(2) ➞ 120
+
+Author: @joshrutkowski
+"""
+
+
+def convert(minutes):
+    pass # Your code here
