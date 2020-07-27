f9812aa42de0a92c80416875630d46fa7c158759
Mark Oliver <mark.c.oliver@gmail.com>
2020-07-27 18:44:44 -0400

    fixed test

--- TopologicalSort/test_topological_sort.py
+++ TopologicalSort/test_topological_sort.py
@@ -1,5 +1,6 @@
 import pytest
-from topological_sort import  *
+from topological_sort import *
+from collections import defaultdict
 
 def test_valid_one():
     tasks = [4, 3, 2, 1]
@@ -10,20 +11,23 @@ def test_valid_one():
         [1, 3],
         [1, 4],
     ]
-    assert is_topol_ordered(topological_sort(tasks, deps), deps) == True
+    ordered = topological_sort(tasks, deps)
+    assert is_topol_ordered(ordered, tasks, deps) == True
 
 
 def test_valid_two():
     tasks = [1, 2, 3, 4, 5]
     deps = [[1, 3], [2, 3], [3, 4], [2, 5], [5, 4]]
-    assert is_topol_ordered(topological_sort(tasks, deps), deps) == True
+    ordered = topological_sort(tasks, deps)
+    assert is_topol_ordered(ordered, tasks, deps) == True
 
 
 def test_valid_three():
     tasks = [1, 2, 3, 4, 5, 6, 7, 8]
     deps = [[3, 1], [8, 1], [8, 7], [5, 7], [
         5, 2], [1, 4], [1, 6], [1, 2], [7, 6]]
-    assert is_topol_ordered(topological_sort(tasks, deps), deps) == True
+    ordered = topological_sort(tasks, deps)
+    assert is_topol_ordered(ordered, tasks, deps) == True
 
 
 def test_invalid():
@@ -34,19 +38,21 @@ def test_invalid():
     assert len(topological_sort(tasks, deps)) == 0
 
 
-def is_topol_ordered(tasks, deps):
-    deps_graph = {}
+def is_topol_ordered(ordered, tasks, deps):
+    deps_graph = defaultdict(list)
+    if len(set(ordered).difference(set(tasks))) != 0:
+        return False
+
     for d in deps:
         if d[1] in deps_graph:
             deps_graph[d[1]].append(d[0])
-        else:
-            deps_graph[d[1]] = [d[0]]
-    for t in tasks:
+
+    for t in ordered:
         if t not in deps_graph:
             return False
         if len(deps_graph[t]):
             return False
-        for d in deps_graph.values():
-            if t in d:
-                d.remove(t)
+        for k, d in deps_graph:
+            if t == d:
+                deps_graph[k].remove(t)
     return True
