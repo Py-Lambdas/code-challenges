9745bbd5c84ec8fa739165a9a113a68181dcffc5
BrannanC <brannanconrad@gmail.com>
2020-08-17 16:04:58 -0500

    adds tests and readme section for add_epitaphs

--- 100_essentials/greetings/README.md
+++ 100_essentials/greetings/README.md
@@ -1,5 +1,9 @@
 # Greetings
 
+Everything you need to complete this challenge can be found [here](https://docs.python.org/3.8/tutorial/controlflow.html#defining-functions).
+
+---
+
 Define a function named `greet` that takes in a name and a message as inputs and returns a greeting in the following format:
 
 ```python
@@ -14,28 +18,32 @@ Additionally, the message should default to "Hello" if none is provided.
 'Hello, Dave!'
 ```
 
-Functions can be passed to other functions. Define a function named `greet_with_style` that takes a name as a positional parameter, plus an arbitrary number of functions, an optional kew word parameter named `msg` that defaults to 'Hello', another optional key word parameter named `sep` that defaults to ', ', and a required keyword parameter named `punctuation`. `greet_with_style` should return a greeting (name + sep + message + puncuation) with all functions applied to it.
+Define a function named `greet_with_style` that takes a name as a required positional parameter, an arbitrary amount of positional parameters, an optional key word parameter named `msg` that defaults to 'Hello', another optional key word parameter named `sep` that defaults to ', ', and a required keyword parameter named `punctuation`. `greet_with_style` should return a greeting (name + sep + message + puncuation) with any functions passed as additional positional arguments applied to it.
 
 ```python
 >>> def starify(s):
 ...    return "*".join(list(s))
+...
 >>> def capitalize(s):
 ...    return s.upper()
->>> greet_with_style("Citizen", "Hello", starify, capitalize, puncuation="!", sep='*')
+...
+>>> greet_with_style("Citizen", starify, capitalize, msg="Greetings", puncuation="!", sep='*')
 'G*R*E*E*T*I*N*G*S***C*I*T*I*Z*E*N*!'
 ```
 
-- add_epitaphs(name, \*\*kwargs)
-- Doc strings and annotations - add to greet_with_style
+Define a function name `add_epitaphs` that will add epitaphs to a name. `add_epitaphs` should take a name and an arbitrary amount of epitaphs as inputs and return a string that is a name with all epitaphs added to it in the folowwing format:
 
 ```python
-# TESTS
-greet_with_style("Citizen", "Hello", starify, puncuation="!", sep='*')
-'H*e*l*l*o*,* *C*i*t*i*z*e*n*!'
+>>> add_epitaphs("Gregson", Son="Greg", Leader="Kobolds")
+'Gregson, Son of Greg, Leader of Kobolds'
 
-greet_with_style(add_epitaphs("Citizen", **epitaphs), puncuation="!", shout=True)
-"HELLO, CITIZEN, SON OF GREG, LEADER OF KOBOLDS!"
+>>> shout = lambda x: x.upper()
+>>> greet_with_style(add_epitaphs(
+...        "Gregson", Son="Greg", Leader="Kobolds"), shout, msg="Hello", punctuation="!")
+'HELLO, GREGSON, SON OF GREG, LEADER OF KOBOLDS!'
 ```
+
+- Doc strings and annotations - add to greet_with_style
 
 - bonus: positional only? What's a use case for positional only (a,b,/,x,y)?
 - bonus: functions can return functions
--- 100_essentials/greetings/test_greetings.py
+++ 100_essentials/greetings/test_greetings.py
@@ -1,7 +1,10 @@
-from inspect import getfullargspec
+from inspect import getfullargspec, signature
 import unittest
 
 from greetings import greet, greet_with_style, add_epitaphs
+
+
+def cap(x): return x.upper()
 
 
 class GreetingsTest(unittest.TestCase):
@@ -13,41 +16,99 @@ class GreetingsTest(unittest.TestCase):
         argspec = getfullargspec(greet)
         self.assertEqual(len(argspec.args), 2,
                          msg="Greet should accept 2 arguments")
-        self.assertIsNotNone(argspec.defaults,
-                             msg="Message should default to 'Hello'")
+        self.assertIsNotNone(
+            argspec.defaults, msg="Message should default to 'Hello'")
         self.assertEqual(len(argspec.defaults), 1,
                          msg="Name should be required")
         self.assertEqual(argspec.defaults[0], "Hello")
-        self.assertEqual(greet("Greg"), "Hello, Greg!",
-                         msg="Greeting should be formatted as message + ', ' + name + '!'")
-        self.assertEqual(greet("Greg", "Greetings"), "Greetings, Greg!",
-                         msg="Greeting should be formatted as message + ', ' + name + '!'")
+        self.assertEqual(
+            greet("Greg"),
+            "Hello, Greg!",
+            msg="Greeting should be formatted as message + ', ' + name + '!'",
+        )
+        self.assertEqual(
+            greet("Greg", "Greetings"),
+            "Greetings, Greg!",
+            msg="Greeting should be formatted as message + ', ' + name + '!'",
+        )
 
     def test_greet_with_style(self):
         argspec = getfullargspec(greet_with_style)
-        self.assertEqual(len(argspec.args), 1,
-                         msg="greet_withstyle should accept 1 positional argument")
+        self.assertEqual(
+            len(argspec.args),
+            1,
+            msg="greet_withstyle should accept 1 positional argument",
+        )
         self.assertNotEqual(
-            argspec.varargs, None, msg="greet_with_style should accept an arbitrary amount of positional arguments with *args")
-        self.assertEqual(len(argspec.kwonlyargs), 3,
-                         msg="greet_with_style should have 3 key word only parameters - msg, sep and punctuation")
-        kwonlys = {'msg', 'sep', 'punctuation'}
+            argspec.varargs,
+            None,
+            msg="greet_with_style should accept an arbitrary amount of positional arguments with *args",
+        )
+        self.assertEqual(
+            len(argspec.kwonlyargs),
+            3,
+            msg="greet_with_style should have 3 key word only parameters - msg, sep and punctuation",
+        )
+        kwonlys = {"msg", "sep", "punctuation"}
         self.assertEqual(len(kwonlys - set(argspec.kwonlyargs)), 0)
-        self.assertIsNotNone(argspec.kwonlydefaults,
-                             msg="Message should default to 'Hello' and the seperator should default to ', '")
+        self.assertIsNotNone(
+            argspec.kwonlydefaults,
+            msg="Message should default to 'Hello' and the seperator should default to ', '",
+        )
         self.assertTrue(
-            'msg' in argspec.kwonlydefaults and 'sep' in argspec.kwonlydefaults, msg="Message should default to 'Hello' and the seperator should default to ', '")
-        self.assertTrue(argspec.kwonlydefaults['msg'] == 'Hello' and argspec.kwonlydefaults['sep']
-                        == ', ', msg="Message should default to 'Hello' and the seperator should default to ', '")
-        self.assertEqual(greet_with_style("Mark", msg="Oh, hi",
-                                          sep=" ", punctuation="."), "Oh, hi Mark.", msg="greet_with_style should assemble a greeting as msg + sep + name + punctuation")
-        self.assertEqual(greet_with_style(
-            "Citizen", lambda x: x.upper(), punctuation="!"), "HELLO, CITIZEN!", msg="greet_with_style should return a greeting after applying all functions passed to *args")
+            "msg" in argspec.kwonlydefaults and "sep" in argspec.kwonlydefaults,
+            msg="Message should default to 'Hello' and the seperator should default to ', '",
+        )
+        self.assertTrue(
+            argspec.kwonlydefaults["msg"] == "Hello"
+            and argspec.kwonlydefaults["sep"] == ", ",
+            msg="Message should default to 'Hello' and the seperator should default to ', '",
+        )
+        self.assertEqual(
+            greet_with_style("Mark", msg="Oh, hi", sep=" ", punctuation="."),
+            "Oh, hi Mark.",
+            msg="greet_with_style should assemble a greeting as msg + sep + name + punctuation",
+        )
+        self.assertEqual(
+            greet_with_style("Citizen", cap, punctuation="!"),
+            "HELLO, CITIZEN!",
+            msg="greet_with_style should return a greeting after applying all functions passed to *args",
+        )
 
         def starify(s):
             return "*".join(list(s))
-        self.assertEqual(greet_with_style(
-            "Citizen", starify, lambda x: x.upper(), msg="Greetings", punctuation="!", sep="*"), "G*R*E*E*T*I*N*G*S***C*I*T*I*Z*E*N*!", msg="greet_with_style should return a greeting after applying all functions passed to *args")
+
+        self.assertEqual(
+            greet_with_style(
+                "Citizen",
+                starify,
+                cap,
+                msg="Greetings",
+                punctuation="!",
+                sep="*",
+            ),
+            "G*R*E*E*T*I*N*G*S***C*I*T*I*Z*E*N*!",
+            msg="greet_with_style should return a greeting after applying all functions passed to *args",
+        )
+
+    def test_add_epitaphs(self):
+        sig = signature(add_epitaphs)
+        params = list(sig.parameters)
+        self.assertEqual(
+            len(params), 2, msg="add_epitaphs should have 2 parameters")
+        name, kwargs = params
+        self.assertEqual(
+            sig.parameters[name].kind, sig.parameters[name].POSITIONAL_OR_KEYWORD
+        )
+        self.assertEqual(
+            sig.parameters[kwargs].kind, sig.parameters[name].VAR_KEYWORD)
+        self.assertEqual(
+            add_epitaphs("Gregson", Son="Greg", Leader="Kobolds"),
+            "Gregson, Son of Greg, Leader of Kobolds",
+            msg="add_epitaphs should add ', {key} of {value}' for all epitaphs in **kwargs to name and return it",
+        )
+        self.assertEqual(greet_with_style(add_epitaphs(
+            "Gregson", Son="Greg", Leader="Kobolds"), cap, msg="Hello", punctuation="!"), "HELLO, GREGSON, SON OF GREG, LEADER OF KOBOLDS!")
 
 
 if __name__ == "__main__":
